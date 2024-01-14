efbed437d45bfa3f4097b0191836096d21b593ff
Gabriel Oliveira <gabriel.oliveira@axented.com>
2024-01-14 17:07:09 -0600

    Finish basic implementation of SQL helpers

--- backend/index.php
+++ backend/index.php
@@ -1,17 +1,10 @@
 <?php
 
 
+use ShoppingCart\Database;
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
-/*
-$stmt = $pdo->query('SELECT * FROM item');
 
-// Fetch all rows as an associative array
-$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// Output or process the results
-foreach ($items as $item) {
-    // Process each item
-    print_r($item);
-}
-*/
+$db = new Database;
+var_dump($db->getCart(1));
--- backend/src/Database.php
+++ backend/src/Database.php
@@ -9,12 +9,12 @@ class Database
 {
     private static $pdo;
 
-    private function __construct()
+    public function __construct()
     {
-        // Private constructor to prevent creating multiple instances
+        self::getInstance();
     }
 
-    public static function getInstance()
+    private static function getInstance()
     {
         if (!self::$pdo) {
             // Configure PDO connection parameters
@@ -34,11 +34,45 @@ class Database
 
                 // Additional configurations if needed
             } catch (PDOException $e) {
-                echo "Connection failed: " . $e->getMessage();
+                throw new  \Exception("Connection failed: " . $e->getMessage());
             }
 
         }
+    }
 
-        return self::$pdo;
+    public function getCart($id)
+    {
+        $query = "SELECT * FROM carts WHERE cart_id = :id";
+        $statement = self::$pdo->prepare($query);
+        $statement->bindParam(':id', $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $cart = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if ($cart) {
+            $result = self::getItemsByCartID($id);
+            $cart['items'] = $result;
+            $cart['subtotal'] = array_sum(array_column($result, 'total'));
+        }
+        return $cart;
+    }
+
+    public function getItemsByCartID($cartID)
+    {
+        $query = "SELECT 
+                    items.*,
+                    cart_items.items_count,
+                    cart_items.items_count * items.price AS total
+                FROM 
+                    items
+                JOIN 
+                    cart_items ON items.item_id = cart_items.item_id
+                WHERE 
+                    cart_items.cart_id = :cart_id;";
+        $statement = self::$pdo->prepare($query);
+        $statement->bindParam(':cart_id', $cartID, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 }
