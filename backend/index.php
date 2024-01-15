58ac2601623b9c1038857e70928228508a0d5ccd
Gabriel Oliveira <gabriel.oliveira@axented.com>
2024-01-15 12:32:12 -0600

    Finished editing types and mutations

--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,18 @@
 <?php
 
 
-use ShoppingCart\Database;
+global $queryType, $mutationType;
+
+use GraphQL\Type\Schema;
+
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
+require_once  __DIR__ . '/src/types.php';
+require_once  __DIR__ . '/src/resolvers.php';
 
-$db = new Database;
+$schema = new Schema([
+    'query' => $queryType,
+    'mutation' => $mutationType,
+]);
+
--- backend/src/types.php
+++ backend/src/types.php
@@ -2,6 +2,7 @@
 
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Definition\ObjectType;
+use ShoppingCart\Database;
 
 $itemType = new ObjectType([
     'name' => 'Item',
@@ -14,18 +15,79 @@ $itemType = new ObjectType([
         'amount' => Type::nonNull(Type::int()),
     ],
 ]);
+
 $cartItemType = new ObjectType([
-    'name' => 'CartItemType',
+    'name' => 'CartItem',
     'fields' => [
         'id' => Type::nonNull(Type::id()),
         'items_count' => Type::nonNull(Type::int()),
         'item' => Type::nonNull($itemType),
+        'total' => Type::nonNull(Type::int()),
     ],
 ]);
+
 $cartType = new ObjectType([
-    'name' => 'CartType',
+    'name' => 'Cart',
     'fields' => [
         'id' => Type::nonNull(Type::id()),
         'cart_items' => Type::nonNull(Type::listOf(Type::nonNull($cartItemType))),
+        'subtotal' => Type::nonNull(Type::int()),
+    ],
+]);
+
+$queryType = new ObjectType([
+    'name' => 'Query',
+    'fields' => [
+        'getCart' => [
+            'type' => $cartType,
+            'args' => [
+                'cartID' => Type::nonNull(Type::id()),
+            ],
+            'resolve' => function ($rootValue, $args, $context) {
+                (new Database())->getCart($args['cartID']);
+            },
+        ],
+        'getItems' => [
+            'type' => Type::nonNull(Type::listOf(Type::nonNull($itemType))),
+            'resolve' => function () {
+                (new Database())->getCatalog();
+            },
+        ],
+    ],
+]);
+
+$mutationType = new ObjectType([
+    'name' => 'Mutation',
+    'fields' => [
+        'addToCart' => [
+            'type' => Type::nonNull(Type::boolean()),
+            'args' => [
+                'cartID' => Type::nonNull(Type::id()),
+                'itemID' => Type::nonNull(Type::id()),
+                'amount' => Type::nonNull(Type::int()),
+            ],
+            'resolve' => function ($rootValue, $args, $context) {
+                (new Database())->addCartItem($args['cartID'], $args['itemID'], $args['amount']);
+            },
+        ],
+        'updateCartItem' => [
+            'type' => Type::nonNull(Type::boolean()),
+            'args' => [
+                'cartItemID' => Type::nonNull(Type::id()),
+                'amount' => Type::nonNull(Type::int()),
+            ],
+            'resolve' => function ($rootValue, $args, $context) {
+                (new Database())->updateCartItem($args['cartItemID'],$args['amount']);
+            },
+        ],
+        'deleteCartItem' => [
+            'type' => Type::nonNull(Type::boolean()),
+            'args' => [
+                'cartItemID' => Type::nonNull(Type::id()),
+            ],
+            'resolve' => function ($rootValue, $args, $context) {
+                (new Database())->deleteCartItem($args['cartItemID']);
+            },
+        ],
     ],
 ]);
