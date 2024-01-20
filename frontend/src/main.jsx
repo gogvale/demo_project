cece1fa62e454c274f7ceb31e2a7364ee48c240e
Gabriel Oliveira <gogvale2011@gmail.com>
2024-01-20 14:13:46 -0600

    Add relay with create-relay-app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import './index.css'
+import { RelayEnvironmentProvider } from "react-relay";
+import { RelayEnvironment } from "./RelayEnvironment";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 import Catalog from "./pages/Catalog.jsx";
 import Item from "./pages/Item.jsx";
 import Cart from "./pages/Cart.jsx";
 import Navbar from "./pages/Navbar.jsx";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Navbar />,
+    children: [
+      {
         path: "/",
-        element: <Navbar/>,
-        children: [
-            {
-                path: "/",
-                element: <Catalog/>,
-            },
-            {
-                path: "items",
-                element: <Catalog/>,
-            },
-            {
-                path: "items/:item_id",
-                element: <Item/>,
-            },
-            {
-                path: "cart",
-                element: <Cart/>,
-            },
-        ]
-    },
-
+        element: <Catalog />,
+      },
+      {
+        path: "items",
+        element: <Catalog />,
+      },
+      {
+        path: "items/:item_id",
+        element: <Item />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RelayEnvironmentProvider environment={RelayEnvironment}>
     <React.StrictMode>
-        <>
-            <RouterProvider router={router}/>
-        </>
-    </React.StrictMode>,
-)
+      <>
+        <RouterProvider router={router} />
+      </>
+    </React.StrictMode>
+  </RelayEnvironmentProvider>
+);
