790fd92215737689ec75ae6b0b73556bd12f7d75
Gabriel Oliveira <gogvale2011@gmail.com>
2024-01-22 02:17:34 -0600

    Add ErrorMessage component

--- frontend/src/components/Popup.jsx
+++ frontend/src/components/Popup.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
@@ -17,16 +17,24 @@ function Popup({ message, onClose, className }) {
 Popup.propTypes = {
     message: PropTypes.string.isRequired,
     onClose: PropTypes.func.isRequired,
+    className: PropTypes.string,
 };
 
 const StyledPopup = styled(Popup)`
     opacity: 0;
     transition: opacity 0.5s ease-in-out;
-    background-color: #4caf50;
-    color: white;
     padding: 10px;
     margin-top: 10px;
     border-radius: 5px;
+    color: white;
+
+    &.success {
+        background-color: #4caf50; // Green color for success messages
+    }
+
+    &.error {
+        background-color: #f44336; // Red color for error messages
+    }
 
     &.show {
         opacity: 1;
@@ -34,7 +42,21 @@ const StyledPopup = styled(Popup)`
 `;
 
 const SuccessMessage = ({ message, onClose }) => (
-    <StyledPopup className="show" message={message} onClose={onClose} />
+    <StyledPopup className="show success" message={message} onClose={onClose} />
 );
 
-export { SuccessMessage };
+SuccessMessage.propTypes = {
+    message: PropTypes.string.isRequired,
+    onClose: PropTypes.func.isRequired,
+};
+
+const ErrorMessage = ({ message, onClose }) => (
+    <StyledPopup className="show error" message={message} onClose={onClose} />
+);
+
+ErrorMessage.propTypes = {
+    message: PropTypes.string.isRequired,
+    onClose: PropTypes.func.isRequired,
+};
+
+export { SuccessMessage, ErrorMessage };
